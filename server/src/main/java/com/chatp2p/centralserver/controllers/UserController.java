d2d7f6aab09be9b6bd485ede1d9e0c832fb9bfbb
Marcos Schlick <marcosschlick@gmail.com>
2025-06-17 17:32:06 -0300

    feat: adiciona User Controller

---
+++ server/src/main/java/com/chatp2p/centralserver/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.chatp2p.centralserver.controllers;
+
+import com.chatp2p.centralserver.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/online")
+    public ResponseEntity<List<Map<String, String>>> getOnlineUsers() {
+        List<Map<String, String>> users = userRepository.findOnlineUsers().stream()
+                .map(user -> {
+                    Map<String, String> userData = new HashMap<>();
+                    userData.put("username", user.getUsername());
+                    userData.put("profileImageUrl", user.getProfileImageUrl());
+                    return userData;
+                })
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(users);
+    }
+}
