9b789ab5ddb21d7f8624d21143410feea64f10eb
Marcos Schlick <marcosschlick@gmail.com>
2025-06-17 17:31:03 -0300

    feat: cria entidade User

---
+++ server/src/main/java/com/chatp2p/centralserver/entities/User.java
@@ -0,0 +1,72 @@
+package com.chatp2p.centralserver.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    private Boolean online = false;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    public User() {
+    }
+
+    public User(String username, String passwordHash, String profileImageUrl) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Boolean getOnline() {
+        return online;
+    }
+
+    public void setOnline(Boolean online) {
+        this.online = online;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+}
