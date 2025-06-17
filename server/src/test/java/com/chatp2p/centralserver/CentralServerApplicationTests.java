21f68d6053b30267f88dbfc47b5221cf3fccf7d1
Marcos Schlick <marcosschlick@gmail.com>
2025-06-17 17:32:29 -0300

    chore: adiciona arquivos de suporte e inicialização

---
+++ server/src/test/java/com/chatp2p/centralserver/CentralServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatp2p.centralserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
