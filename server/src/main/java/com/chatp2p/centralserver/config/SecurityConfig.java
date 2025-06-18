7a735f03f4379452811be39c6c3a75542f36942c
Marcos Schlick <marcosschlick@gmail.com>
2025-06-18 16:19:27 -0300

    refactor: atualizar configuração de segurança para endpoints protegidos

--- server/src/main/java/com/chatp2p/centralserver/config/SecurityConfig.java
+++ server/src/main/java/com/chatp2p/centralserver/config/SecurityConfig.java
@@ -3,19 +3,28 @@ package com.chatp2p.centralserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-        );
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS)
+                );
+
         return http.build();
     }
 
