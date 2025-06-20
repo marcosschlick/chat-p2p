42599d137d9ad5cbd14caf0f1d3e01005f773f91
Marcos Schlick <marcosschlick@gmail.com>
2025-06-19 21:56:18 -0300

    feat: criar classe principal e model de mensagem

---
+++ desktop/src/main/java/com/chatp2p/Message.java
@@ -0,0 +1,38 @@
+package com.chatp2p;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    public enum MessageType {
+        TEXT, FILE, CONNECTION_REQUEST, CONNECTION_ACCEPTED
+    }
+
+    private String sender;
+    private String recipient;
+    private String content;
+    private String fileName;
+    private byte[] fileData;
+    private MessageType type;
+
+    public Message(String sender, String recipient, String content, MessageType type) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = content;
+        this.type = type;
+    }
+
+    public Message(String sender, String recipient, String fileName, byte[] fileData, MessageType type) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.fileName = fileName;
+        this.fileData = fileData;
+        this.type = type;
+    }
+
+    public String getSender() { return sender; }
+    public String getRecipient() { return recipient; }
+    public String getContent() { return content; }
+    public String getFileName() { return fileName; }
+    public byte[] getFileData() { return fileData; }
+    public MessageType getType() { return type; }
+}
