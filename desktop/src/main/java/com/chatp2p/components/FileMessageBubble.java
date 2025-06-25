974281f844cdfe363099411664b4ed7fd82b2e01
Marcos Schlick <marcosschlick@gmail.com>
2025-06-25 12:18:21 -0300

    refactor(components): revisão de simplicidade em bubbles de mensagem

--- desktop/src/main/java/com/chatp2p/components/FileMessageBubble.java
+++ desktop/src/main/java/com/chatp2p/components/FileMessageBubble.java
@@ -9,20 +9,13 @@ import javafx.scene.layout.VBox;
 public class FileMessageBubble extends MessageBubble {
     public FileMessageBubble(String fileName, boolean sent) {
         super("", sent);
-
         VBox bubble = (VBox) getChildren().get(0);
-
         Image clipIcon = new Image(getClass().getResourceAsStream("/com/chatp2p/icons/paperclip.png"));
-
-        HBox container = new HBox();
-        container.setSpacing(5);
-
+        HBox container = new HBox(5);
         ImageView iconView = new ImageView(clipIcon);
         iconView.setFitWidth(16);
         iconView.setFitHeight(16);
-
         container.getChildren().addAll(iconView, new Label(fileName));
-
         bubble.getChildren().set(0, container);
     }
 }
