a7d0fa15ed5f1659f793dae088a1a935beef64a4
Marcos Schlick <marcosschlick@gmail.com>
2025-06-25 12:18:21 -0300

    feat(models): adiciona classe UserProfile simplificada

---
+++ desktop/src/main/java/com/chatp2p/models/UserProfile.java
@@ -0,0 +1,43 @@
+package com.chatp2p.models;
+
+public class UserProfile {
+    private Long id;
+    private String username;
+    private String profileImageUrl;
+    private String authToken;
+
+    public UserProfile() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+}
