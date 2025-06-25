102bf54549e2a4970c949bafab088a20aa53e1ea
Marcos Schlick <marcosschlick@gmail.com>
2025-06-25 12:18:21 -0300

    refactor(utils): padroniza tratamento de erros de rede com exceções personalizadas

--- desktop/src/main/java/com/chatp2p/utils/NetworkUtils.java
+++ desktop/src/main/java/com/chatp2p/utils/NetworkUtils.java
@@ -1,15 +1,22 @@
 package com.chatp2p.utils;
 
+import com.chatp2p.exceptions.*;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class NetworkUtils {
     public static String getLocalIP() {
-        try (DatagramSocket socket = new DatagramSocket()) {
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket();
             socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
             return socket.getLocalAddress().getHostAddress();
         } catch (Exception e) {
-            return "127.0.0.1";
+            throw new NetworkException("Failed to get local IP address", e);
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
         }
     }
 }
