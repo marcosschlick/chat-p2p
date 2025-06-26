bf27969f3a24d8551f59ffac0d7f6415497aeb65
Marcos Schlick <marcosschlick@gmail.com>
2025-06-26 10:34:15 -0300

    refactor(h2): mudar diretório de MessageRepository

--- desktop/src/main/java/com/chatp2p/repositories/MessageRepository.java
+++ desktop/src/main/java/com/chatp2p/repositories/MessageRepository.java
@@ -1,15 +1,17 @@
-package com.chatp2p.managers;
+package com.chatp2p.repositories;
 
+import com.chatp2p.managers.DatabaseManager;
 import com.chatp2p.models.Message;
+import com.chatp2p.models.MessageType;
+
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MessageRepository {
 
     public void save(Message msg, Long senderId, Long recipientId) {
-        if (msg.getType() != Message.MessageType.TEXT && msg.getType() != Message.MessageType.FILE) {
+        if (msg.getType() != MessageType.TEXT && msg.getType() != MessageType.FILE) {
             return;
         }
 
@@ -18,7 +20,7 @@ public class MessageRepository {
             ps.setLong(1, senderId);
             ps.setLong(2, recipientId);
             ps.setString(3, msg.getType().name());
-            ps.setString(4, msg.getType() == Message.MessageType.FILE ? msg.getFileName() : msg.getContent());
+            ps.setString(4, msg.getType() == MessageType.FILE ? msg.getFileName() : msg.getContent());
             ps.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Falha ao gravar histórico", e);
@@ -27,43 +29,41 @@ public class MessageRepository {
 
     public List<Message> findHistory(Long userA, Long userB) {
         List<Message> messages = new ArrayList<>();
-        String sql = "SELECT sender_id, type, content, created_at FROM messages " +
-                    "WHERE (sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?) " +
-                    "ORDER BY created_at ASC";
-        
+        String sql = "SELECT sender_id, type, content, created_at FROM messages " + "WHERE (sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?) " + "ORDER BY created_at ASC";
+
         try (PreparedStatement ps = DatabaseManager.getConnection().prepareStatement(sql)) {
             ps.setLong(1, userA);
             ps.setLong(2, userB);
             ps.setLong(3, userB);
             ps.setLong(4, userA);
-            
+
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
                     Long senderId = rs.getLong("sender_id");
                     String type = rs.getString("type");
                     String content = rs.getString("content");
                     Timestamp timestamp = rs.getTimestamp("created_at");
-                    
-                    Message.MessageType messageType = Message.MessageType.valueOf(type);
+
+                    MessageType messageType = MessageType.valueOf(type);
                     Message message = new Message();
                     message.setType(messageType);
                     message.setContent(content);
-                    
+
                     boolean isSent = senderId.equals(userA);
                     message.setSender(isSent ? "Você" : "Outro");
                     message.setRecipient(isSent ? "Outro" : "Você");
-                    
-                    if (messageType == Message.MessageType.FILE) {
+
+                    if (messageType == MessageType.FILE) {
                         message.setFileName(content);
                     }
-                    
+
                     messages.add(message);
                 }
             }
         } catch (SQLException e) {
             throw new RuntimeException("Falha ao carregar histórico", e);
         }
-        
+
         return messages;
     }
 } 
